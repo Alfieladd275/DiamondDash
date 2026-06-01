4f92cb5bdae1d06144ff8b0bc48f1dca25a5f3a1
Alfieladd275 <773525@newcollege.ac.uk>
2026-06-01 15:00:22 +0100

    Laser experiments/ money pickups working

--- Assets/Scripts/Money Pickup.cs
+++ Assets/Scripts/Money Pickup.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
@@ -14,22 +14,22 @@ public class CoinPickup : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            // 1. Play the sound through the Player's AudioSource for clarity
+            // ✅ Play coin sound through player
             AudioSource playerAudio = other.GetComponent<AudioSource>();
             if (coinSound != null && playerAudio != null)
             {
-                // We use a slightly higher pitch for coins to make them sound "extra" shiny
                 playerAudio.pitch = Random.Range(1.1f, 1.2f);
                 playerAudio.PlayOneShot(coinSound, volume);
-
-                // Reset pitch so other sounds don't stay high-pitched
                 playerAudio.pitch = 1.0f;
             }
 
-            // 2. Log the collection (or add to your Inventory/GameManager here)
-            Debug.Log("Collected " + coinValue + " coin(s)!");
+            // ✅ Add score to UI
+            if (ScoreManager.Instance != null)
+            {
+                ScoreManager.Instance.AddScore(coinValue);
+            }
 
-            // 3. Destroy the coin
+            // ✅ Destroy coin
             Destroy(gameObject);
         }
     }
---
+++ Assets/Scripts/Score Feed.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager Instance;
+
+    public int score = 0;
+    public TextMeshProUGUI scoreText;
+
+    private int displayedScore = 0;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void AddScore(int amount)
+    {
+        score += amount;
+        StopAllCoroutines(); // stop previous animation if collecting fast
+        StartCoroutine(AnimateScore());
+    }
+
+    IEnumerator AnimateScore()
+    {
+        while (displayedScore < score)
+        {
+            displayedScore++;
+            scoreText.text = displayedScore.ToString();
+            yield return new WaitForSeconds(0.001f); // speed of counting
+        }
+    }
+}
